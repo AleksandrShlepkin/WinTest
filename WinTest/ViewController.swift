be3a3eeb3dc088db41b1d1ce0fe8fe064890dd07
Aleksandr Korotkov <81365352+AleksandrShlepkin@users.noreply.github.com>
2023-07-19 17:35:51 +0300

    Initial Commit

---
+++ WinTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WinTest
+//
+//  Created by Mac on 19.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
